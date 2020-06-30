097ed52fc47b26b3653f578752660631afd39915
marceloppf <57159435+marceloppf@users.noreply.github.com>
2020-06-30 20:10:53 -0300

    assets\icon.png assets\splash.png

---
+++ src/pages/Veiculos.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import { StyleSheet, Text, View } from 'react-native'
+
+export default function Veiculos() {
+    return (
+        <View>
+            <Text>VEICULOS</Text>
+        </View>
+    )
+}
+
+const styles = StyleSheet.create({})

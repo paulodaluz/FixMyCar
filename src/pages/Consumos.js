90026fe7c12ad909f8af25771ab2008bb53d4659
marceloppf <57159435+marceloppf@users.noreply.github.com>
2020-07-01 13:13:40 -0300

    src\pages\Consumos.js

---
+++ src/pages/Consumos.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import { StyleSheet, Text, View } from 'react-native'
+
+export default function Consumos() {
+    return (
+        <View>
+            <Text>CONSUMOS</Text>
+        </View>
+    )
+}
+
+const styles = StyleSheet.create({})

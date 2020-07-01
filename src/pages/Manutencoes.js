747279af1631e0d757069c14005d7fbc6efe6d03
marceloppf <57159435+marceloppf@users.noreply.github.com>
2020-07-01 13:12:45 -0300

    assets\icon_orig.png assets\splash_orig.png src\img\icon.png src\img\splash.png src\pages\Consumos.js src\pages\Manutencoes.js

---
+++ src/pages/Manutencoes.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import { StyleSheet, Text, View } from 'react-native'
+
+export default function Manutencoes() {
+    return (
+        <View>
+            <Text>MANUTENCOES</Text>
+        </View>
+    )
+}
+
+const styles = StyleSheet.create({})
